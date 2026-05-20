24f3467f70d7497017da0d94fe4894e897634934
Ishika <ishikacodershive@gmail.com>
2026-05-20 14:05:18 +0530

    Bust cart script cache

--- server.js
+++ server.js
@@ -1301,7 +1301,7 @@ function layout({ title, description = "", currentPath = "/", content, user = nu
       ${renderCromaFooter()}
     </div>
     <script>window.__MAPLE_THEME__=${JSON.stringify(theme || "snow")};</script>
-    <script src="/public/app.js"></script>
+    <script src="/public/app.js?v=cart-total-20260520"></script>
   </body>
   </html>`;
 }
